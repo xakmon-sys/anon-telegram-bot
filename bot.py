b094c53bdaa64b064600d321f7739f26ea3d69e9
xakmon-sys <xakmon@icloud.com>
2026-05-19 04:37:51 +0300

    Fix incomplete import statement in bot.py
    Removed an incomplete import statement.

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiohttp import web
 
 BOT_TOKEN = "8724564645:AAFk2Im7H2_WpY9G9EiRZbnIz1fRuwa_4J4"
-impor
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
